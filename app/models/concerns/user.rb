19ce0402e0abeba24e008d6bee4b42fa9159136f
Career Closet for CS 606 <zhangyuelinchina@gmail.com>
2018-03-13 18:08:34 +0000

    add user validation

--- app/models/concerns/user.rb
+++ app/models/concerns/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
-    
+    validates :username, presence: true
+    validates :UIN, presence: true, uniqueness: true,
+              length:{minimum: 9, maximum: 9}
+    VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true,
+              length: {maximum: 105},
+              uniqueness: true,
+              format: { with: VALID_EMAIL_REGEX}
 end
