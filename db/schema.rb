b8b37bd147cca779a8f3f251951d64654b2ac3da
Career Closet for CS 606 <zhangyuelinchina@gmail.com>
2018-03-13 17:46:52 +0000

    Create user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313064324) do
+ActiveRecord::Schema.define(version: 20180313173943) do
 
   create_table "suits", force: :cascade do |t|
     t.string   "number"
     t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "UIN"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
